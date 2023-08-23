6ccc0ca9280478a228faa5d92ae834eca082c822
Shivansh Raghav <shivanshraghav7@gmail.com>
2023-08-24 02:10:16 +0530

    sub

---
+++ src/components/Tour.js
@@ -0,0 +1,31 @@
+import React,{useState} from 'react'
+
+let Tour = ({arr,set})=>{
+    let[info,Setinfo]=useState()
+    function changedata(id){
+        arr.forEach((element,index)=>{
+            if(element.id==id){
+                let temparr=[...arr]
+                temparr.splice(index,1)
+                set(temparr)
+            }
+        })
+    }
+    console.log(arr[0].info.split(' ').length)
+    return (
+        arr.map(element=>{
+            return (
+                <div className='single-tour'>
+                    <div className='title'>{element.name}</div>
+                    <div className='tour-info'>{element.info}</div>
+                    <div className='tour-price'>{element.price}
+                    <button></button>
+                    </div>
+                    <img src={element.image} alt='temp' ></img>
+                    <button className='delete-btn' onClick={()=>{changedata(element.id)}}>Delete Tour</button>
+                </div>
+            )
+        })
+    )
+}
+export default Tour
